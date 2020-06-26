ca25df0bd5039f931b73b2112aa482a5b0852fe1
yami-er <queenyani@gmail.com>
2020-06-25 22:08:04 -0400

    final push

--- demo/src/main/java/com/example/demo/Payroll/LoadDatabase.java
+++ demo/src/main/java/com/example/demo/Payroll/LoadDatabase.java
@@ -7,16 +7,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class LoadDatabase {
+public class LoadDatabase {
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
-	@Bean
-	CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
 
-		return args -> {
-			log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
-			log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
-		};
-	}
+            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> {
+                log.info("Preloaded " + order);
+            });
+
+        };
+    }
 }
